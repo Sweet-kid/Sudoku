2fad483c357dc7645a9e237a104a2a598c85a806
Upasana <me@upasana.me>
2014-12-02 02:09:40 +0530

    Function for checking if the number if present in the block of 3

--- solution_in_perl.pl
+++ solution_in_perl.pl
@@ -118,3 +118,52 @@ sub is_present_vertically {
     }
     return 0;
 }
+
+sub test_is_present_in_the_block_of_3 {
+    my $array_ref = [
+        [  1,  2,  3,  4,  5,  6 ],
+        [  7,  8,  9, 10, 11, 12 ],
+        [ 13, 14, 15, 16, 17, 18 ],
+        [ 19, 20, 21, 22, 23, 24 ],
+        [ 25, 26, 27, 28, 29, 30 ],
+        [ 31, 32, 33, 34, 35, 36 ]
+        ];
+
+    if( is_present_in_next_3_col_row( 4, $array_ref, 6, 6, 4, 2 ) ) {
+        print "4 is present in the block of 3, co-ordinates(4, 2)\n";
+    } else {
+        print "4 is not present in the block of 3, co-ordinates(4, 2)\n";
+    }
+    if( is_present_in_next_3_col_row( 4, $array_ref, 6, 6, 2, 2 ) ) {
+        print "4 is present in the block of 3, co-ordinates(2, 2)\n";
+    } else {
+        print "4 is not present in the block of 3, co-ordinates(2, 2)\n";
+    }
+}
+
+sub is_present_in_the_block_of_3 {
+    my $number      = shift;
+    my $y_lines_ref = shift;
+    my $x           = shift;
+    my $y           = shift;
+    my $i           = shift;
+    my $j           = shift;
+
+    my @y_lines = @$y_lines_ref;
+
+    my $modulo_x = $j % 3;
+    my $modulo_y = $i % 3;
+
+    for my $index_y( 0 .. 2 ) {
+        for my $index_x( 0 .. 2 ) {
+            my $x_lines_ref = $y_lines[ $index_y + $i - $modulo_y ];
+            my @x_lines     = @$x_lines_ref;
+            my $element = $x_lines[ $index_x + $j - $modulo_x ];
+            if( $element eq $number ) {
+                return 1;
+            }
+        }
+    }
+
+    return 0;
+}
