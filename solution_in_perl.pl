03a57c200df17ebf9281c4bef642ca9938f6547c
Upasana <me@upasana.me>
2014-12-02 01:02:16 +0530

    Wrote function to initialize the array of solutions for each co-ordinate

--- solution_in_perl.pl
+++ solution_in_perl.pl
@@ -16,10 +16,28 @@ for my $i( 0 .. $x - 1 ) {
     $y_lines[ $i ] = \@x_lines;
 }
 
+
+my $solutions_ref = initialize_three_d_array_of_solutions( $x, $y );
+
 sub prompt {
     my $message = shift;
     print "\n$message : ";
     my $var = <STDIN>;
     $var =~ s/\s//;
     return $var;
+}
+
+sub initialize_three_d_array_of_solutions {
+    my $x = shift;
+    my $y = shift;
+    my @y_lines;
+
+    for my $i ( 0 .. $y - 1 ) {
+        my @x_lines;
+        for my $j ( 0 .. $x - 1 ) {
+            $x_lines[ $j ] = [ 1 .. 9 ];
+        }
+        $y_lines[ $i ] = \@x_lines;
+    }
+    return \@y_lines;
 }
